1dbb5bf1e033e89afa2f6e97a4a3a98db81570fa
alanmasu <alan.masutti@gmail.com>
2026-08-02 13:18:04 +0200

    feat: Implemented getSystemNumberOfActors() and initSystem() + Replica Constructor
    Fixes #4

--- src/main/java/it/unitn/ds/Replica.java
+++ src/main/java/it/unitn/ds/Replica.java
@@ -5,7 +5,14 @@ import akka.actor.Props;
 
 import java.util.Optional;
 
+import java.util.Map;
+
 public class Replica extends AbstractReplica {
+    
+    private Map<Integer, ActorRef> groupOfReplicas;
+
+    int positions[];
+    int coordinatorID;
 
     public Replica(int id) {
         this(id, AbstractReplica.MIN_LATENCY, AbstractReplica.MAX_LATENCY, AbstractReplica.COORDINATOR_BEAT_INTERVAL, Optional.empty());
@@ -13,7 +20,7 @@ public class Replica extends AbstractReplica {
 
     public Replica(int id, int minLatency, int maxLatency, int coordinatorBeatInterval, Optional<ActorRef> listener) {
         super(id, minLatency, maxLatency, coordinatorBeatInterval, listener);
-        // TODO: implement
+        positions = new int[AbstractReplica.POSITIONS_LIST_LENGTH];
     }
 
     public static Props props(int id, int minLatency, int maxLatency, int coordinatorBeatInterval) {
@@ -24,20 +31,21 @@ public class Replica extends AbstractReplica {
     public static Props propsWithListener(int id, int minLatency, int maxLatency, int coordinatorBeatInterval, ActorRef listener) {
         return Props.create(Replica.class, () -> new Replica(id, minLatency, maxLatency, coordinatorBeatInterval, Optional.ofNullable(listener)));
     }
+    
+    @Override
+    public void initSystem(InitSystem sysInit) {
+        this.groupOfReplicas = sysInit.group; 
+        this.coordinatorID = sysInit.coordinator_id;
+        log("Initialized with group of replicas: " + getSystemNumberOfActors() + " replicas, coordinator ID: " + coordinatorID);
+    }
 
     @Override
     public int getSystemNumberOfActors() {
-        // TODO: implement
-        return 0;
+        return this.groupOfReplicas.size();
     }
 
     @Override
     public void crash(AbstractReplica.Crash how_to_crash) {
-        // TODO: implement
-    }
-
-    @Override
-    public void initSystem(InitSystem sysInit) {
         // TODO: implement
     }
 
