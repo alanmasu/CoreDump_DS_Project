2bbb17bd727d326b0afcb499a7e8f6ccb9df3920
alanmasu <alan.masutti@gmail.com>
2026-08-08 15:44:17 +0200

    refactor: Change owner type + added common attributes
     - I changed the 'owner' type from ActorRef to Actor to be uses as 'this' when the transaction will be created and this should be used only as a 'computation' unit infact is ment for be passed as a message.
     - I added the common attributes like the 'id' and the 'history'
    
    Related to #44

--- src/main/java/it/unitn/ds/Transaction.java
+++ src/main/java/it/unitn/ds/Transaction.java
@@ -1,16 +1,20 @@
 package it.unitn.ds;
 
 import akka.actor.Cancellable;
-import akka.actor.ActorRef;
+import akka.actor.Actor;
 
+import java.util.List;
 
 public abstract class Transaction{
 
     protected Cancellable timeout;
-    protected ActorRef owner;
+    protected Actor owner;
     protected EpochPair epochPair;
+    public int id;
 
-    Transaction(ActorRef owner, Cancellable timeout) {
+    List<Msg> history;
+
+    Transaction(Actor owner, Cancellable timeout) {
         this.owner = owner;
         this.timeout = timeout;
         this.epochPair = null;
@@ -29,4 +33,10 @@ public abstract class Transaction{
      * @param msg the message to process and compute the new state.
      */
     abstract void computeState(Msg msg);
+
+    /**
+     * This method is called when a new coordinator is elected. 
+     * It allows the transaction to perform any necessary actions to conclude the transactions previously handled by the old coordinator.
+     */
+    abstract void onCoordinatorElected();
 }
