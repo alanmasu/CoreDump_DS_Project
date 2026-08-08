c9225618fbaf0259906aaae28b3f6ac0e1f7119f
alanmasu <alan.masutti@gmail.com>
2026-08-08 19:00:45 +0200

    feat: Added the equals, toString and hashCode methods + updated tests

--- src/main/java/it/unitn/ds/EpochPair.java
+++ src/main/java/it/unitn/ds/EpochPair.java
@@ -19,6 +19,30 @@ public class EpochPair implements Comparable<EpochPair> {
         }
     }
 
+    @Override
+    public String toString() {
+        return "<" + epoch + ", " + sequence + ">";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        EpochPair other = (EpochPair) obj;
+        return this.epoch == other.epoch && this.sequence == other.sequence;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(epoch);
+        result = 31 * result + Integer.hashCode(sequence);
+        return result;
+    }
+
     public int getEpoch() {
         return epoch;
     }
--- src/test/java/it/unitn/ds/regression/TestEpochPair.java
+++ src/test/java/it/unitn/ds/regression/TestEpochPair.java
@@ -1,5 +1,6 @@
 package it.unitn.ds.regression;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,22 @@ public class TestEpochPair {
     void testEpochPairComparison() {
         EpochPair pair1 = new EpochPair(1, 1);
         EpochPair pair2 = new EpochPair(1, 2);
+        EpochPair pair3 = new EpochPair(1, 1); 
+
+
         assertTrue(pair1.compareTo(pair2) < 0);
         assertTrue(pair2.compareTo(pair1) > 0);
         assertTrue(pair1.compareTo(pair1) == 0);
         assertTrue(pair2.compareTo(pair2) == 0);
+        assertTrue(pair3.compareTo(pair1) == 0);
+        assertTrue(pair1.compareTo(pair3) == 0);
+        assertTrue(pair1.equals(pair1));
+        assertFalse(pair1.equals(pair2));
+        assertTrue(pair3.equals(pair1));
+
+        assertTrue(pair1.hashCode() == pair3.hashCode());
+        assertFalse(pair1.hashCode() == pair2.hashCode());
+
     }
     
 }
