acd41c5e60a075a97ab469fd1f00f25f02895f58
alanmasu <alan.masutti@gmail.com>
2026-08-09 22:57:02 +0200

    feat: Enhance Msg class with additional attributes and override methods
    Related to alanmasu/CoreDump_DS_Project#47

--- src/main/java/it/unitn/ds/Msg.java
+++ src/main/java/it/unitn/ds/Msg.java
@@ -1,7 +1,52 @@
 package it.unitn.ds;
 import java.io.Serializable;
 
+import akka.actor.ActorRef;
+
 
 public abstract class Msg implements Serializable {
+    public final int ownerId;
+    public final int ownerTransactionId;
+    public final EpochPair epochPair;
+
+    public final ActorRef sender;
+    
+
+    public Msg(int ownerId, int ownerTransactionId, EpochPair epochPair, ActorRef sender) {
+        this.ownerId = ownerId;
+        this.ownerTransactionId = ownerTransactionId;
+        this.epochPair = epochPair;
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Msg{" +
+                "ownerId=" + ownerId +
+                ", ownerTransactionId=" + ownerTransactionId +
+                ", epochPair=" + epochPair +
+                ", sender=" + sender.path().name() +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(ownerId);
+        result = 31 * result + Integer.hashCode(ownerTransactionId);
+        result = 31 * result + (epochPair != null ? epochPair.hashCode() : 0);
+        result = 31 * result + (sender != null ? sender.path().name().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Msg other = (Msg) obj;
+        return this.ownerId == other.ownerId &&
+               this.ownerTransactionId == other.ownerTransactionId &&
+               (this.epochPair != null ? this.epochPair.equals(other.epochPair) : other.epochPair == null) &&
+               (this.sender != null ? this.sender.path().name().equals(other.sender.path().name()) : other.sender == null);
+    }
 
 };
