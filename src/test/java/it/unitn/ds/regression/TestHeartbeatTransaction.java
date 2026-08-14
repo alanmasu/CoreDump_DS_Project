b8ebadbcfb1d61fd98201bedd086b99444a97af3
Tech-Matt <mattia.rizzo.un@gmail.com>
2026-08-14 17:06:12 +0300

    test: Add unit test for heartbeat tick routing by transaction ID

--- src/test/java/it/unitn/ds/regression/TestHeartbeatTransaction.java
+++ src/test/java/it/unitn/ds/regression/TestHeartbeatTransaction.java
@@ -13,6 +13,7 @@ import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
 import it.unitn.ds.AbstractReplica.InitSystem;
 import it.unitn.ds.HeartbeatTransaction.HeartbeatMsg;
+import it.unitn.ds.HeartbeatTransaction.HeartbeatTickMsg;
 import it.unitn.ds.Transaction.TransactionId;
 import it.unitn.ds.Replica;
 
@@ -96,6 +97,86 @@ public class TestHeartbeatTransaction {
                 secondHeartbeat.sender
             );
 
+        } finally {
+            TestKit.shutdownActorSystem(system);
+        }
+    }
+
+    /**
+     * Verifies that Replica dispatches heartbeat ticks using their transaction ID.
+     * 
+     * A tick with an unknown transaction ID must not produce a heartbeat.
+     */
+    @Test
+    void heartbeatTickIsRoutedByTransactionId() {
+        ActorSystem system = ActorSystem.create(
+            "heartbeatTickIsRoutedByTransactionId"
+        );
+
+        try {
+            int coordinatorId = 0;
+            int heartbeatIntervalMillis = 5000;
+
+            TestKit followerProbe = new TestKit(system);
+
+            ActorRef coordinator = system.actorOf(
+                Replica.props(
+                    coordinatorId,
+                    1,
+                    5,
+                    heartbeatIntervalMillis
+                ),
+                "coordinator"
+            );
+
+            Map<Integer, ActorRef> group = new HashMap<>();
+            group.put(coordinatorId, coordinator);
+            group.put(1, followerProbe.getRef());
+
+            TransactionId activeTransactionId = new TransactionId(
+                coordinator,
+                0
+            );
+
+            TransactionId unknownTransactionId = new TransactionId(
+                coordinator,
+                999
+            );
+
+            coordinator.tell(
+                new InitSystem(group, coordinatorId),
+                ActorRef.noSender()
+            );
+
+            coordinator.tell(
+                new HeartbeatTickMsg(
+                    unknownTransactionId,
+                    coordinator
+                ),
+                ActorRef.noSender()
+            );
+
+            followerProbe.expectNoMessage(
+                Duration.ofMillis(200)
+            );
+
+            coordinator.tell(
+                new HeartbeatTickMsg(
+                    activeTransactionId,
+                    coordinator
+                ),
+                ActorRef.noSender()
+            );
+
+            HeartbeatMsg heartbeat = followerProbe.expectMsgClass(
+                Duration.ofSeconds(2),
+                HeartbeatMsg.class
+            );
+
+            assertEquals(
+                activeTransactionId,
+                heartbeat.transactionId
+            );
         } finally {
             TestKit.shutdownActorSystem(system);
         }
