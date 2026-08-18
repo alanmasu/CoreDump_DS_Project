8c42a1808432b4cdc7c212c75784d8c0fda9a1be
alanmasu <alan.masutti@gmail.com>
2026-08-18 21:07:29 +0200

    test: Added testing with timeout

--- src/test/java/it/unitn/ds/regression/TestWriteTransaction.java
+++ src/test/java/it/unitn/ds/regression/TestWriteTransaction.java
@@ -2,7 +2,8 @@ package it.unitn.ds.regression;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -13,6 +14,9 @@ import akka.actor.Actor;
 import akka.actor.ActorRef;
 import it.unitn.ds.AbstractClient;
 import it.unitn.ds.AbstractClient.WriteResult;
+import it.unitn.ds.AbstractClient.WriteTimeout;
+import it.unitn.ds.AbstractReplica;
+import it.unitn.ds.AbstractReplica.Crash;
 import it.unitn.ds.WriteTransaction.WriteFinishMsg;
 import it.unitn.ds.Client;
 import it.unitn.ds.Logger;
@@ -27,8 +31,28 @@ import akka.testkit.javadsl.TestKit;
  */
 class TestWriteTransaction {
     
-    @BeforeAll
-    static void setup() {
+    final static int NODES_N = 3;
+    final static int COORDINATOR_ID = 0;
+    TestsSystemWrapper sys;
+    TestKit probe;
+    ActorRef replica;
+    ActorRef client;
+
+    @AfterEach
+    void teardown() {
+        sys.system.terminate();
+    }
+
+    @BeforeEach
+    void setup() {
+        sys = TestsCommons.createTestSystem("oneClientWrite_" + COORDINATOR_ID, NODES_N, COORDINATOR_ID);
+        probe = new TestKit(sys.system);
+        replica = sys.actors.get(0);
+        client = sys.system.actorOf(   Client.propsWithListener(sys.client_read_timeout, 
+                                                                         sys.client_write_timeout,
+                                                                         Optional.ofNullable(sys.actors.get(0)),
+                                                                         probe.getRef()),
+                                                "client1");
         Logger.setDestinationStdout();
         Logger.setDebugEnabled(true);
         Logger.setLoggingEnabled(true);
@@ -36,18 +60,6 @@ class TestWriteTransaction {
 
     @Test
     void testSimpleWriteTransaction(){
-        final int n_nodes = 3;
-        final int coordinator = 0;
-        final TestsSystemWrapper sys = TestsCommons.createTestSystem("oneClientWrite_" + coordinator, n_nodes, coordinator);
-        setup();
-		TestKit probe = new TestKit(sys.system);
-        ActorRef replica = sys.actors.get(0);
-		ActorRef client = sys.system.actorOf(   Client.propsWithListener(sys.client_read_timeout, 
-                                                                         sys.client_write_timeout,
-                                                                         Optional.ofNullable(sys.actors.get(0)),
-                                                                         probe.getRef()),
-				                                "client1");
-
 		client.tell(new AbstractClient.WriteRequest(TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, replica), Actor.noSender());
 
         TransactionId clientTransactionId = new TransactionId(client, 0);
@@ -62,8 +74,21 @@ class TestWriteTransaction {
         assertEquals(TestsCommons.TEST_VALUE, result.value, "The value written should be the same as the one requested");
         assertEquals(TestsCommons.TEST_INDEX, result.index, "The index written should be the same as the one requested");
         assertEquals(true, result.success, "The write should be successful");
-		
-		sys.system.terminate();
 	}
     
+
+    @Test
+    void testWriteTransactionTimeout(){
+        Crash replicaCrash = new Crash(Crash.Type.Now, 0);
+        replica.tell(replicaCrash, ActorRef.noSender());
+        client.tell(new AbstractClient.WriteRequest(TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, replica), Actor.noSender());
+            
+        probe.expectNoMessage(Duration.ofMillis(TestsCommons.getClientWriteTimeout(AbstractReplica.MAX_LATENCY, sys.getNNodes())));
+        WriteTimeout result = probe.expectMsgClass(WriteTimeout.class);
+        assertEquals(TestsCommons.TEST_INDEX, result.index, "The index written should be the same as the one requested");
+        assertEquals(client, result.client, "The client should be the same as the one requested");
+        assertEquals(replica, result.replica, "The replica should be the same as the one requested");
+        sys.system.terminate();
+    }
+
 }
