926f05df9ec33424761a57d8193230df5fb7a2bb
alanmasu <alan.masutti@gmail.com>
2026-08-22 22:29:28 +0200

    fix: Change TestHeartbeatTransaction from public to package-private

--- src/test/java/it/unitn/ds/regression/TestHeartbeatTransaction.java
+++ src/test/java/it/unitn/ds/regression/TestHeartbeatTransaction.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
-public class TestHeartbeatTransaction {
+class TestHeartbeatTransaction {
 
     @Test
     void coordinatorBroadcastsHeartbeatsPeriodically() {
